155b611f3aa99cbd71c67203e41bfe5800534ecb
Aaron Clare <AaronClare@Aarons-MacBook-Air-2.local>
2019-09-02 13:15:15 -0700

    Updates

---
+++ precache-manifest.6591bba4871ca17591c91af24eaf54e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c9bc793598f9e272887eec7a8a48da2",
+    "url": "/ReactLyricFinder/index.html"
+  },
+  {
+    "revision": "d43106f995b5327b98d8",
+    "url": "/ReactLyricFinder/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "ad71ba689ce835c6cde4",
+    "url": "/ReactLyricFinder/static/js/2.029b3d8a.chunk.js"
+  },
+  {
+    "revision": "d43106f995b5327b98d8",
+    "url": "/ReactLyricFinder/static/js/main.1d1fb272.chunk.js"
+  },
+  {
+    "revision": "33bad6de39df05e3f6aa",
+    "url": "/ReactLyricFinder/static/js/runtime~main.401b51db.js"
+  }
+]);
